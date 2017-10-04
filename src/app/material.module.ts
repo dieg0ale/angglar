2264a4768fbd4de79513564424c82fdd2c37d834
Diego Cabra <diegocabra@hotmail.com>
2017-10-04 15:50:10 +0200

    angular components 2

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -6,6 +6,7 @@ import {
   MatToolbarModule,
   MatIconModule,
   MatCardModule,
+  MatGridListModule,
   MatListModule
 } from '@angular/material';
 
@@ -16,6 +17,7 @@ import {
     MatToolbarModule,
     MatIconModule,
     MatCardModule,
+    MatGridListModule,
     MatListModule
   ],
   exports: [
@@ -24,6 +26,7 @@ import {
     MatToolbarModule,
     MatIconModule,
     MatCardModule,
+    MatGridListModule,
     MatListModule
   ]
 })
